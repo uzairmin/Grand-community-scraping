86297baae914041952ac27d4f79914d47e7dcbbf
uzairmin <uzair.am10@gmail.com>
2024-05-28 04:50:29 +0500

    done scraping

--- instagram_accounts_scraping.py
+++ instagram_accounts_scraping.py
@@ -1,40 +1,38 @@
 import time
 from typing import List
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
 import pandas as pd
-
 from helpers import configure_webdriver
+import os
 
-def find_posts(driver):
+def find_posts(driver, scraped_jobs, count):
     try:
-        actions = ActionChains(driver)
-        scraped_jobs:List[dict] = []
-        posts = driver.find_elements(By.CLASS_NAME, "card-contain")
-        for post in posts:
-            try:
-                data: dict = {}
-                data['username'] = post.text.split('\n')[1]
+        driver.get(f"https://grand-community.com/en/dashboard/$brand/influencers/{ count }")
+        time.sleep(1)
+        data: dict = {}
+        data["username"] = "N/A"
+        data["interests"] = "N/A"
+        data["instagram_followers"] = "N/A"
+        data["tiktok_followers"] = "N/A"
+        data["engagement_rate"] = "N/A"
+        
+        data['username'] = driver.find_element(By.TAG_NAME, "h2").text
+        data['interests'] = driver.find_elements(By.CLASS_NAME, "direction")[0].text.strip("Intrest\n").replace("\n", ", ")
+        social_media = driver.find_elements(By.CLASS_NAME, "v-chip--clickable")
+        for media in social_media:
+            if 'instagram' in media.text.lower():
+                media.click()
                 time.sleep(1)
-                social_media = post.find_elements(By.TAG_NAME, "a")
-                for media in social_media:
-                    if 'instagram' in media.get_attribute('href'):
-                        actions.move_to_element(media).perform()
-                        social_media = post.find_elements(By.TAG_NAME, "a")
-                        for s_media in social_media:
-                            if 'instagram' in s_media.get_attribute('href'):
-                                data['followers'] = s_media.text
-                                break
-                if not "K" in data["followers"] and float(data['followers'].split("K")[0]) >= 10 and float(data['followers'].split("K")[0]) <= 200:
-                    data: dict = {}
-                    continue
-                data['engagement_rate'] = post.find_element(By.CLASS_NAME, "font-bold").text
-                scraped_jobs.append(data.copy())
-            except Exception as e:
-                print(e)
-            break
-        df = pd.DataFrame(scraped_jobs)
-        df.to_excel("instagram_accounts_info.xlsx", index=False)
+                data['instagram_followers'] = driver.find_element(By.TAG_NAME, "strong").text
+                data['engagement_rate'] = driver.find_elements(By.TAG_NAME, "strong")[3].text
+
+            elif 'tiktok' in media.text.lower():
+                media.click()
+                time.sleep(1)
+                data['tiktok_followers'] = driver.find_element(By.TAG_NAME, "strong").text
+        
+        scraped_jobs.append(data.copy())
+
     except Exception as e:
         print(e)
 
@@ -59,14 +57,25 @@ def instagram():
         driver = configure_webdriver(True)
         driver.maximize_window()
         try:
-            flag = True
             count = 0
+            scraped_jobs:List[dict] = []
             driver.get("https://grand-community.com/en/dashboard/$brand/influencers")
             import pdb
             pdb.set_trace()
-            while flag:
-                load_posts(driver, count)
-            find_posts(driver)
+            while count < 50000:
+                count+=1
+                find_posts(driver, scraped_jobs, count)
+                if count % 100 == 0:
+                    df = pd.DataFrame(data=scraped_jobs)
+                    filename = "scraped-data.xlsx"
+                    if os.path.exists(filename):
+                        existing_df = pd.read_excel(filename, na_filter=False)
+                        df = pd.concat([existing_df, df], ignore_index=True)
+                    else:
+                        df = df
+                
+                    df.to_excel(filename, index=False)
+                    scraped_jobs:List[dict] = []
         except Exception as e:
             print(e)
         finally:
